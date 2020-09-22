da6cf239d1257f9b864b7d5284893aedfc833961
hermanjakobsen <hermankjakobsen@gmail.com>
2020-09-22 09:36:52 +0200

    Remove redundant Probe object

--- src/my_models/objects/xml_objects.py
+++ src/my_models/objects/xml_objects.py
@@ -1,13 +1,5 @@
 from robosuite.models.objects import MujocoXMLObject
 
-class ProbeObject(MujocoXMLObject):
-    """
-    Probe object
-    """
-
-    def __init__(self):
-        super().__init__("my_models/assets/objects/probe.xml")
-
 class TorsoObject(MujocoXMLObject):
     """
     Torso object
