14f042e132eb97dfcb543384d46d185dd9e5c8b8
Herman Kolstad Jakobsen <hermankjakobsen@gmail.com>
2021-01-25 10:37:04 +0100

    Remove unused demos

--- src/main.py
+++ src/main.py
@@ -18,9 +18,7 @@ from helper import register_gripper, plot_joint_pos, plot_forces_and_contact, pl
 from demos import controller_demo, \
     contact_btw_probe_and_body_demo, \
     standard_mujoco_py_demo, \
-    drop_cube_on_body_demo, \
-    change_parameters_of_soft_body_demo, \
-    fetch_push_gym_demo
+    change_parameters_of_soft_body_demo
 
 
 register_env(Ultrasound)
@@ -31,9 +29,7 @@ register_gripper(UltrasoundProbeGripper)
 #controller_demo('UR5e', save_data=False)
 #contact_btw_probe_and_body_demo(1, 'main', save_data=False)
 #standard_mujoco_py_demo()
-#drop_cube_on_body_demo()
 #change_parameters_of_soft_body_demo(3)
-#fetch_push_gym_demo()
 
 #plot_joint_pos('data/UR5e_joint_pos_controller_test.csv')
 #plot_forces_and_contact('data/main_ee_forces_contact_btw_probe_and_body.csv', 'data/main_contact_contact_btw_probe_and_body.csv')
