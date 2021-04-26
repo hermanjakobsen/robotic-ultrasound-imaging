e003cc7e63a60473967f37ff7210d64dabbbd6c6
Herman Kolstad Jakobsen <hermankjakobsen@gmail.com>
2021-04-26 13:23:21 +0200

    Runnable hmfc

--- src/controllers/hmfc.py
+++ src/controllers/hmfc.py
@@ -27,14 +27,14 @@ class HybridMotionForceController(Controller):
             actuator_range,
         )
 
+        # Control dimension
+        self.control_dim = 0    # x and y position, and force in z-direction
+
         # input and output max and min (allow for either explicit lists or single numbers)
         self.input_max = self.nums2array(input_max, self.control_dim)
         self.input_min = self.nums2array(input_min, self.control_dim)
         self.output_max = self.nums2array(output_max, self.control_dim)
         self.output_min = self.nums2array(output_min, self.control_dim)
-
-        # Control dimension
-        self.control_dim = 3    # x and y position, and force in z-direction
 
         # control frequency
         self.control_freq = policy_freq
@@ -87,13 +87,23 @@ class HybridMotionForceController(Controller):
         self.robot = None
         self.probe_id = None
 
-        # Initialize goals based on initial pos / ori
-        self.goal_ori = T.convert_quat(T.mat2quat(np.array(self.initial_ee_ori_mat)), to="wxyz")    # (w, x, y, z) quaternion
-        self.goal_pos = np.array(self.initial_ee_pos)
+        # Initialize goals 
+        self.r_d_ddot = np.zeros(5)         # position trajectories [placeholder]
+        self.r_d_dot = np.zeros(5)     
+        self.p_d = np.zeros(2)
 
-        # Initialize force measurements
+        self.f_d_ddot = 0                   # force trajectories [placeholder]
+        self.f_d_dot = 0
+        self.f_d = 0
+
+        self.goal_ori = T.convert_quat(np.array([-0.69192486,  0.72186726, -0.00514253, -0.01100909]), to="wxyz")    # (w, x, y, z) quaternion
+
+        # Initialize measurements
+        self.z_force = 0                    # contact force in z-direction
         self.prev_z_force = 0
-        self.z_force = 0
+        
+        self.v = np.zeros(5)                # angular and linear velocity
+
 
 
     def _initialize_measurements(self):
@@ -111,22 +121,14 @@ class HybridMotionForceController(Controller):
     # ------------ Helper functions -------------------------------- # 
 
     def quatdiff_in_euler_radians(self, quat_curr, quat_des):
-        curr_mat = quaternion.as_rotation_matrix(quat_curr)
-        des_mat = quaternion.as_rotation_matrix(quat_des)
+        curr_mat = T.quat2mat(T.convert_quat(quat_curr, to="xyzw"))
+        des_mat = T.quat2mat(T.convert_quat(quat_des, to="xyzw"))
         rel_mat = des_mat.T.dot(curr_mat)
         rel_quat = quaternion.from_rotation_matrix(rel_mat)
         vec = quaternion.as_float_array(rel_quat)[1:]
         if rel_quat.w < 0.0:
             vec = -vec
         return -des_mat.dot(vec)
-
-        
-    # Return the position and (relative) orientation 
-    def get_x(self, p, ori, goal_ori):
-        pos_x = p[:2]
-        rel_ori = self.quatdiff_in_euler_radians(goal_ori, np.asarray(ori))
-        return np.append(pos_x,rel_ori)
-
 
     # Fetch the estimated external forces and torques (h_e / F_ext)
     def construct_h_e(self, Fz):
@@ -152,7 +154,7 @@ class HybridMotionForceController(Controller):
     # Calculate the error in position and orientation (in the subspace subject to motion control)
     def get_delta_r(self, ori, goal_ori, p, p_d, two_dim = True):
         delta_pos = p_d - p[:2]
-        delta_ori = self.quatdiff_in_euler_radians(np.asarray(ori), goal_ori)    
+        delta_ori = self.quatdiff_in_euler_radians(ori, goal_ori)    
         if two_dim == True:
             return np.array([np.append(delta_pos,delta_ori)]).reshape([5,1])
 
@@ -172,6 +174,7 @@ class HybridMotionForceController(Controller):
         lambda_a = f_d_ddot 
         lambda_b = np.array(np.dot(K_Dlambda,(f_d_dot-lambda_dot)))
         lambda_c = np.dot(K_Plambda,(f_d-z_force))
+ 
         return max(lambda_a + lambda_b + lambda_c,0)
 
 
@@ -202,42 +205,58 @@ class HybridMotionForceController(Controller):
         return np.array(np.dot(S_v, alpha_v)).reshape([6,1]) + f_lambda*np.array(np.dot(C_dot,S_f)).reshape([6,1])
 
 
+    def set_goal(self, action):
+        
+        self.r_d_ddot = np.zeros(5)         # position trajectories [placeholder]
+        self.r_d_dot = np.zeros(5)     
+        self.p_d = np.zeros(2)
+
+        self.f_d_ddot = 0                   # force trajectories [placeholder]
+        self.f_d_dot = 0
+        self.f_d = 0
+         
+
     def run_controller(self):
         
-        # position trajectories [placeholder]
-        r_d_ddot = np.zeros(5)
-        r_d_dot = np.zeros(5)     
-        p_d = np.zeros(2)
-
-        # force trajectories [placeholder]
-        f_d_ddot = 0
-        f_d_dot = 0
-        f_d = 0
-
-        # linear and angular velocity [placeholder]
-        v   = np.zeros(5)
-
         # eef measurements
         pos = self.ee_pos
         ori = T.convert_quat(T.mat2quat(self.ee_ori_mat), to="wxyz")    # (w, x, y, z) quaternion
-        self.z_force = self.sim.data.cfrc_ext[self.probe_id][-3:]
+        self.z_force = 0    # [Placeholder]
+        self.v = np.zeros(5).reshape([5, 1]) # [Placeholder]
 
         # torque computations
-        alpha_v = self.calculate_alpha_v(ori, r_d_ddot, r_d_dot, pos, p_d, v) 
-        f_lambda = self.calculate_f_lambda(f_d_ddot, f_d_dot, f_d, self.S_f ,self.C, self.K_Dlambda, self.K_Plambda, self.z_force)
+        alpha_v = self.calculate_alpha_v(ori, self.r_d_ddot, self.r_d_dot, pos, self.p_d, self.v) 
+        #print(f"alpha_v: {alpha_v}")
+        f_lambda = self.calculate_f_lambda(self.f_d_ddot, self.f_d_dot, self.f_d, self.S_f ,self.C, self.K_Dlambda, self.K_Plambda, self.z_force)
+        #print(f"f_lambda: {f_lambda}")
         alpha = self.calculate_alpha(self.S_v, alpha_v, self.C, self.S_f, -f_lambda)
+        #print(f"alpha: {alpha}")
         cartesian_inertia = np.linalg.inv(np.linalg.multi_dot([self.J_full, np.linalg.inv(self.mass_matrix), self.J_full.T]))
 
          # NOTE MODIFIED FROM DEFAULT. Removed external forces and such
-        torque = np.array(np.linalg.multi_dot([self.J_full.T ,cartesian_inertia, alpha])).reshape([7,1])
-
-
-        super().run_controller()
+        torque = np.array(np.linalg.multi_dot([self.J_full.T ,cartesian_inertia, alpha])).flatten() + self.torque_compensation
 
         # update measurements
         self.prev_z_force = self.z_force
+
+        # Always run superclass call for any cleanups at the end
+        super().run_controller()
+
         
         return torque
+
+
+    @property
+    def control_limits(self):
+        """
+        Returns the limits over this controller's action space, overrides the superclass property
+
+            2-tuple:
+                - (np.array) minimum action values
+                - (np.array) maximum action values
+        """
+        low, high = self.input_min, self.input_max
+        return low, high
 
 
     @property
